329bc50c92709c16ae4d2b98bdd28b0a9c07c07a
灰色梦魇 <253596304@qq.com>
2016-12-08 09:28:19 +0800

    2016120

---
+++ obj/local/armeabi/objs/MoonPlayer/MoonPlayer.o.d
@@ -0,0 +1,13 @@
+obj/local/armeabi/objs/MoonPlayer/MoonPlayer.o: jni/MoonPlayer.cpp \
+ D:/adt-bundle-windows-x86_64-20140321/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/jni.h \
+ D:/adt-bundle-windows-x86_64-20140321/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ D:/adt-bundle-windows-x86_64-20140321/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ D:/adt-bundle-windows-x86_64-20140321/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/android/api-level.h
+
+D:/adt-bundle-windows-x86_64-20140321/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/jni.h:
+
+D:/adt-bundle-windows-x86_64-20140321/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
+
+D:/adt-bundle-windows-x86_64-20140321/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
+
+D:/adt-bundle-windows-x86_64-20140321/android-ndk-r9b/platforms/android-19/arch-arm/usr/include/android/api-level.h:
